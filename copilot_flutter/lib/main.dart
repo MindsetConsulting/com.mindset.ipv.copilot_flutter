6bf756febadeeb2e34bb13274d80feb4b15dedcd
Curtis Kettler <curtiskettler@mindsetconsulting.com>
2023-08-04 12:36:53 -0500

    fully functional CRUD app created in less than an hour

--- copilot_flutter/lib/main.dart
+++ copilot_flutter/lib/main.dart
@@ -1,18 +1,307 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MaterialApp(
+    home: MainApp(),
+  ));
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MainApp extends StatefulWidget {
+  const MainApp({Key? key}) : super(key: key);
+
+  @override
+  _MainAppState createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  List<Map<String, dynamic>> _humans = [];
+
+  void _addHuman(String name, int age) {
+    setState(() {
+      _humans.add({'name': name, 'age': age});
+    });
+  }
+
+  void _navigateToAddHuman(BuildContext context) async {
+    final result = await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => AddHumanPage()),
+    );
+    if (result != null) {
+      _addHuman(result['name'], result['age']);
+    }
+  }
+
+  void _navigateToEditHuman(BuildContext context, int index) async {
+    final result = await Navigator.push(
+      context,
+      MaterialPageRoute(
+          builder: (context) => EditHumanPage(human: _humans[index])),
+    );
+    if (result != null) {
+      _editHuman(index, result['name'], result['age']);
+    }
+  }
+
+  void _editHuman(int index, String name, int age) {
+    setState(() {
+      _humans[index]['name'] = name;
+      _humans[index]['age'] = age;
+    });
+  }
+
+  void _deleteHuman(int index) {
+    setState(() {
+      _humans.removeAt(index);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      title: 'Human List',
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          title: const Text('Human List'),
+        ),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  _navigateToAddHuman(context);
+                },
+                child: const Text('Add Human'),
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _humans.length,
+                itemBuilder: (context, index) {
+                  final human = _humans[index];
+                  return ListTile(
+                    title: Text(human['name']),
+                    subtitle: Text('Age: ${human['age']}'),
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        IconButton(
+                          icon: Icon(Icons.edit),
+                          onPressed: () {
+                            _navigateToEditHuman(context, index);
+                          },
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.delete),
+                          onPressed: () {
+                            _deleteHuman(index);
+                          },
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class AddHumanPage extends StatefulWidget {
+  const AddHumanPage({Key? key}) : super(key: key);
+
+  @override
+  _AddHumanPageState createState() => _AddHumanPageState();
+}
+
+class _AddHumanPageState extends State<AddHumanPage> {
+  final _formKey = GlobalKey<FormState>();
+  final _nameController = TextEditingController();
+  final _ageController = TextEditingController();
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _ageController.dispose();
+    super.dispose();
+  }
+
+  void _submitForm() {
+    if (_formKey.currentState!.validate()) {
+      final name = _nameController.text;
+      final age = int.parse(_ageController.text);
+      Navigator.pop(context, {'name': name, 'age': age});
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add Human'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextFormField(
+                controller: _nameController,
+                decoration: const InputDecoration(
+                  labelText: 'Name',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a name';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: _ageController,
+                decoration: const InputDecoration(
+                  labelText: 'Age',
+                ),
+                keyboardType: TextInputType.number,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter an age';
+                  }
+                  final age = int.tryParse(value);
+                  if (age == null || age < 0) {
+                    return 'Please enter a valid age';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 16.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text('Cancel'),
+                  ),
+                  const SizedBox(width: 16.0),
+                  ElevatedButton(
+                    onPressed: _submitForm,
+                    child: const Text('Submit'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class EditHumanPage extends StatefulWidget {
+  final Map<String, dynamic> human;
+
+  const EditHumanPage({Key? key, required this.human}) : super(key: key);
+
+  @override
+  _EditHumanPageState createState() => _EditHumanPageState();
+}
+
+class _EditHumanPageState extends State<EditHumanPage> {
+  final _formKey = GlobalKey<FormState>();
+  final _nameController = TextEditingController();
+  final _ageController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _nameController.text = widget.human['name'];
+    _ageController.text = widget.human['age'].toString();
+  }
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _ageController.dispose();
+    super.dispose();
+  }
+
+  void _submitForm() {
+    if (_formKey.currentState!.validate()) {
+      final name = _nameController.text;
+      final age = int.parse(_ageController.text);
+      Navigator.pop(context, {'name': name, 'age': age});
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Edit Human'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextFormField(
+                controller: _nameController,
+                decoration: const InputDecoration(
+                  labelText: 'Name',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a name';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: _ageController,
+                decoration: const InputDecoration(
+                  labelText: 'Age',
+                ),
+                keyboardType: TextInputType.number,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter an age';
+                  }
+                  final age = int.tryParse(value);
+                  if (age == null || age < 0) {
+                    return 'Please enter a valid age';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 16.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text('Cancel'),
+                  ),
+                  const SizedBox(width: 16.0),
+                  ElevatedButton(
+                    onPressed: _submitForm,
+                    child: const Text('Save'),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
